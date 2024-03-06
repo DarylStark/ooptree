1d994b497074222e7810ca86d6d28973158b32df
Daryl Stark <daryl@dstark.nl>
2024-03-06 12:04:06 +0100

    Add CLI script

--- src/ooptree/__init__.py
+++ src/ooptree/__init__.py
@@ -3,8 +3,38 @@
 A CLI tool to visualize class hierarchies.
 """
 
+from rich.console import Console
+
+from .class_finder import ClassFinder
 from .ooptree import OOPTree
+from .ooptree_printer import OOPTreePrinter
+
+
+def print_tree(
+    base_class_name: str | type,
+    modules: list[str] | None = None,
+) -> None:
+    """Print a tree for a specific class."""
+    if isinstance(base_class_name, str):
+        # Configure the finder
+        finder = ClassFinder(base_class_name)
+        if modules:
+            for module in modules:
+                finder.add_module(module)
+        # Get the base class
+        base_class = finder.get_class()
+    elif isinstance(base_class_name, type):
+        base_class = base_class_name
+    else:
+        raise TypeError('Argument "base_class_name" should be a str or a type')
+
+    # Create a Rich Console
+    console = Console()
+
+    # Print the OOP Tree
+    tree = OOPTree(base_class)
+    printer = OOPTreePrinter(tree)
+    printer.print_to_console(console)
+
 
 __version__ = '0.0.0-dev'
-
-__all__ = ['OOPTree']
--- src/ooptree/__main__.py
+++ src/ooptree/__main__.py
@@ -1,4 +1,22 @@
 """The main module for the CLI script."""
 
+from typing import Annotated
+
+import typer
+
+from . import print_tree
+
+
+def main(
+    base_class_name: str,
+    module: Annotated[
+        list[str],
+        typer.Option(default_factory=list, help='Import specific modules.'),
+    ],
+) -> None:
+    """Print the class hierarchy for a specific class."""
+    print_tree(base_class_name, module)
+
+
 if __name__ == '__main__':
-    pass
+    typer.run(main)
